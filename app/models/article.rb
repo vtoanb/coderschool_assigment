d91dcc3e8ae69f0a9c1ac3cc6cbf9891b95924b1
vtoanb <vtoanb@gmail.com>
2016-10-02 10:30:58 +0700

    validates submit content

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
+	validates :title, presence: true
+	validates :body, presence: true
     def self.search(search)
         where("title ILIKE ?", "%#{search}%")
     end
