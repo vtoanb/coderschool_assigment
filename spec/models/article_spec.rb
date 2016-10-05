7bcb5205d98f91e0f9846229d9dfb6b7a33df361
vtoanb <vtoanb@gmail.com>
2016-10-06 01:12:03 +0700

    change require rails_helper instead of spec_helper

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,7 +1,7 @@
 # spec/models/article.rb
-require 'spec_helper'
+require 'rails_helper'
 
-describle Article do
+describe Article do
 	it "has a valid factory"
 	it "is valid without title"
 	it "is valid without body"
