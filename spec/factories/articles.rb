5db66355adff031b415b8a550a189008cb3d9ca4
vtoanb <vtoanb@gmail.com>
2016-10-06 01:26:45 +0700

    start with factory and faker, now try to run rspec

---
+++ spec/factories/articles.rb
@@ -0,0 +1,9 @@
+# spec/factories/articles.rb
+require 'faker'
+
+FactoryGirl.define do
+	factory :article do |f|
+		f.title { Faker::Title.title }
+		f.body  { Faker::Body.body }
+	end
+end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,8 +1,16 @@
 # spec/models/article.rb
 require 'rails_helper'
 
+# describe Article do
+#	it "has a valid factory"
+#	it "is valid without title"
+#	it "is valid without body"
+# end
+
 describe Article do
-	it "has a valid factory"
-	it "is valid without title"
-	it "is valid without body"
+	it "has a valid factory" do
+		Factory.create(:contact).should be_valid
+	end
+	it "is invalid without title"
+	it "is invalid without body"
 end
