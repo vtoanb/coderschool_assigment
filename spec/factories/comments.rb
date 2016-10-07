5c412986dff52af84a6bad2524739bc566b4f7c7
vtoanb <vtoanb@gmail.com>
2016-10-08 01:02:23 +0700

    temporatory remove one part of test spec

---
+++ spec/factories/comments.rb
@@ -0,0 +1,7 @@
+# spec/factories/comments.rb
+
+require 'faker'
+
+# factories.define do |f|
+#  	f.content "comment content to rspec test"
+# end
---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,12 @@
+# spec/models/comment_spec.rb
+require 'rails_helper'
+
+# describle Comment do
+#	it "has valid factory" do
+#		expect(FactoryGirl.create(:comment)).to be_valid
+#	end
+#
+#	is "invalid without content" do
+#		expect(FactoryGirl.build(:comment,content: nil)).not_to be_valid
+#	end
+# end
